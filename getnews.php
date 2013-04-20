9b6beec415cda644d209667d52d04f5b47c0af60
sampurcell93 <spurcell93@gmail.com>
2013-04-20 15:16:41 -0400

    pull from gnews

--- getnews.php
+++ getnews.php
@@ -1,15 +1,16 @@
 <?php
 	$q = urlencode($_GET['q']);
-	$url = "https://ajax.googleapis.com/ajax/services/search/news?" .
-	       "v=1.0&q=" . $q;
-
-	// sendRequest
-	// note how referer is set manually
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_REFERER, "http://localhost");
-	$body = curl_exec($ch);
-	curl_close($ch);
+	$start = urlencode($_GET['start']);
+	function pull($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_REFERER, "http://localhost");
+		$body = curl_exec($ch);
+		curl_close($ch);
+ 		return $body;
+	}
+	$body = pull( "https://ajax.googleapis.com/ajax/services/search/news?" . "v=1.0&start=" . $start . "&q=" . $q);
 	echo $body;
-	// now have some fun with the results...
+	// $json = json_decode($body);
+	// $totalpages = count($json->responseData->cursor->pages);
--- storymap.js
+++ storymap.js
@@ -8,28 +8,46 @@ $(document).ready(function() {
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
         	mapOptions);
-	};
-	// if (navigator.geolocation) {
- //        navigator.geolocation.getCurrentPosition(function(position) {
- //                console.log(position);
- //                Map = new Map();
- //        }); 
-	// }
-	// else 
-	Map = new Map();
+        this.mapOptions = mapOptions;
+        this.map = map;
+	};	
+	Map.prototype.plotStories = function(stories) {
+		console.log(stories.responseData);
 
-	$("#news-search").focus().on("keydown",function(e) {
-		if (e.keyCode == 13 || e.which == 13)
-			$("#go").trigger("click");
+	}
+	Map = new Map();
+	var $search = $("#news-search");
+	var $go = $("#go");
+
+	$search.focus().data("start_index",0).on("keydown",function(e) {
+		if (e.keyCode == 13 || e.which == 13){
+			$go.trigger("click");
+			return;
+		}
+		$(this).data("start_index", $(this).data("start_index") + 1);
 	});
 
-	$("#go").on("click", function() {
+	$go.on("click", function() {
+		getNews(0);
+		getData();
+	})
+
+	function getNews(start) {
 		$.get("./getnews.php", 
 			{
-				q: $("#news-search").val(),
-				since: "1970-01-01"
-			}, function(data) {
-			console.log(data);
-		})
-	})
+				q: $search.val(),
+				start: start
+			},
+		function(data) {
+			Map.plotStories(JSON.parse(data));
+		});
+	}
+	function getData() {
+		var content = " yolo";
+		var clfsws = new XMLHttpRequest();
+        clfsws.open("POST", "http://api.opencalais.com/enlighten/rest/", true);
+        clfsws.onreadystatechange = function() { console.log("here")};
+        clfsws.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        clfsws.send("licenseID=c3wjfrkfmrsft3r5wgxm5skr&content=" + encodeURIComponent(content) + '&paramsXML=' + encodeURIComponent('<c:params xmlns:c="http://s.opencalais.com/1/pred/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><c:processingDirectives c:contentType="text/txt" c:outputFormat="text/gnosis" c:discardMetadata=";"></c:processingDirectives><c:userDirectives c:allowDistribution="true" c:allowSearch="true" c:externalID="calaisbridge" c:submitter="calaisbridge"></c:userDirectives><c:externalMetadata c:caller="GnosisFirefox"/></c:params>'));
+	}
 })
