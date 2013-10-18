599b84df18cc7e477e4068a0cc740979d7c7c4d3
Samuel Purcell <Samuel.Purcell@tufts.edu>
2013-10-18 09:14:42 -0400

    restructured application for flask backend - Calais still NOT working, be careful. If someone wants to rewrite iti n python that'd be great.

--- SMBackend/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/newsmaps?unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock'
-db = SQLAlchemy(app)	
-from app import views, models
